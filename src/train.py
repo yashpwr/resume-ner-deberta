b7d39f379d4a0e69996095f9c5904c05408cab30
yashpwr <asyncyash@gmail.com>
2025-08-12 08:55:14 +0530

    fix: correct evaluation strategy parameter name in training script

--- src/train.py
+++ src/train.py
@@ -410,7 +410,7 @@ class ResumeNERTrainer:
             label_smoothing_factor=self.config['training'].get('label_smoothing', 0.05),
             
             # Evaluation
-            evaluation_strategy="epoch",
+            eval_strategy="epoch",
             save_strategy="epoch",
             load_best_model_at_end=True,
             metric_for_best_model="f1",
